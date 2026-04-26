d48761d1a658d661e3235b9a331bb0e70d5eb262
Maira Noor <mairanoor.mn@gmail.com>
2026-04-26 17:21:16 +0500

    Initial commit with hospital system and tests

--- basic/meow.py
+++
@@ -1,9 +0,0 @@
-print("maira")
-
-def name(n):
-    print(n)
-
-name("maira")
-
-chai_one = "milk tea"
-chai_two = "green tea"
--- basic/meow2.py
+++
@@ -1,4 +0,0 @@
-from meow import name 
-import meow
-name(meow.chai_one)
-
